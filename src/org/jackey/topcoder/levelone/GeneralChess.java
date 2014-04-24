b8bd982a96277d6ee2fc78a93dca9ece49b369e4
jackey90 <jackey90.hj@gmail.com>
2014-04-24 17:11:07 +0800

    GeneralChess

--- src/org/jackey/topcoder/levelone/GeneralChess.java
+++ src/org/jackey/topcoder/levelone/GeneralChess.java
@@ -34,33 +34,72 @@ import java.util.List;
 public class GeneralChess {
     public String[] attackPositions(String[] pieces) {
         List<String> list = new ArrayList<String>();
-        int[][] realCoords = new int[4][4];
-        int[][] mockCoords = new int[4][4];
-
 
         if (pieces != null) {
-            int[] realXs = new int[pieces.length];
-            int[] realYs = new int[pieces.length];
+            int[][] realCoords = new int[pieces.length][2];
             int xMax = Integer.MIN_VALUE;
             int xMin = Integer.MAX_VALUE;
             int yMax = Integer.MIN_VALUE;
             int yMin = Integer.MAX_VALUE;
+
+            int[][] visited = new int[7][7];
+
             for (int i = 0; i < pieces.length; i++) {
                 String[] coord = pieces[i].split(",");
-                realXs[i] = Integer.parseInt(coord[0]);
-                realYs[i] = Integer.parseInt(coord[1]);
-                xMax = xMax < realXs[i] ? realXs[i] : xMax;
-                xMin = xMin > realXs[i] ? realXs[i] : xMin;
-                yMax = yMax < realYs[i] ? realYs[i] : yMax;
-                yMin = yMin > realYs[i] ? realYs[i] : yMin;
+                realCoords[i][0] = Integer.parseInt(coord[0]);
+                realCoords[i][1] = Integer.parseInt(coord[1]);
+                xMax = xMax < realCoords[i][0] ? realCoords[i][0] : xMax;
+                xMin = xMin > realCoords[i][0] ? realCoords[i][0] : xMin;
+                yMax = yMax < realCoords[i][1] ? realCoords[i][1] : yMax;
+                yMin = yMin > realCoords[i][1] ? realCoords[i][1] : yMin;
             }
+
+            int[][] mockCoords = new int[pieces.length][2];
+            int xMid = (xMax + xMin) / 2;
+            int yMid = (yMax + yMin) / 2;
             if ((xMax - xMin <= 4) && (yMax - yMin <= 4)) {
+                int coordXMax = xMid - xMin + 2;
+                int coordXMin = xMid - xMin - 2;
+                int coordYMax = yMid - yMin + 2;
+                int coordYmin = yMid - yMin - 2;
+
+                int[][] move = {{1, 2}, {2, 1}, {-1, -2}, {-2, -1}, {-1, 2}, {1, -2}, {-2, 1}, {2, -1}};
+                for (int i = 0; i < pieces.length; i++) {
+                    mockCoords[i][0] = realCoords[i][0] - xMin + 2;
+                    mockCoords[i][1] = realCoords[i][1] - yMin + 2;
+
+                    for (int j = 0; j < 8; j++) {
+                        int x = mockCoords[i][0] + move[j][0];
+                        int y = mockCoords[i][1] + move[j][1];
+                        if (x >= coordXMin + 2 && x <= coordXMax + 2 && y <= coordYMax + 2 && y >= coordYmin + 2) {
+                            visited[x][y] += 1;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < visited.length; i++) {
+                    for (int j = 0; j < visited[i].length; j++) {
+                        if (visited[i][j] == pieces.length) {
+                            list.add((i + xMin - 2) + "," + (j + yMin - 2));
+                        }
+                    }
+                }
 
             }
 
         }
+        String[] result = new String[list.size()];
+        return list.toArray(result);
+    }
 
-        return (String[]) list.toArray();
+    public static void main(String[] args) {
+        String[] str = {"-1000,1000", "-999,999", "-999,997"};
+        String[] result = new GeneralChess().attackPositions(str);
+
+        for (String coord : result) {
+            System.out.print(coord);
+        }
+
     }
 
 
