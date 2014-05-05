ddfc280411f4afd15281798233d33af85135153d
jackey90 <jackey90.hj@gmail.com>
2014-05-05 14:30:28 +0800

    WeirdRooks

--- src/org/jackey/topcoder/leveltwo/WeirdRooks.java
+++ src/org/jackey/topcoder/leveltwo/WeirdRooks.java
@@ -5,6 +5,7 @@ package org.jackey.topcoder.leveltwo;
  * User: jinhuang
  * Date: 4/30/14
  * Time: 12:27 PM
+ *  solved by :https://gist.github.com/ishikawa/18241#file-weirdrooks-java-L29
  * To change this template use File | Settings | File Templates.
  * Problem Statement
  * On a weird chess board, each row can have a different number of columns. Element k of cols will give the number of columns in row k. Each row is flush left, so the right side can look quite ragged. In a valid assignment of n rooks to the weird chess board, no two rooks can share a row or column. In such an assignment, an unoccupied square is considered special if there is no rook to its right in the same row and no rook below in the same column (element 0 of cols describes the highest row). You are going to return a String containing a single-space delimited list of pairs. The pair (quotes for clarity) "r,f" should appear in the final string if and only if there is a valid assignment with r rooks such that f squares are special. The pairs should be sorted in nondecreasing order by r values. If a tie occurs, the lower f value should come first. The returned value should contain no repeated pairs. See the examples for further clarifications.
@@ -52,5 +53,54 @@ package org.jackey.topcoder.leveltwo;
  * Returns: "0,11 1,2 1,3 1,4 1,5 1,6 1,7 1,8 1,9 1,10 2,0 2,1 2,2 2,3 2,4 2,5 2,6 2,7 2,8"
  */
 public class WeirdRooks {
+    private int[] columns;
+    private int n;
+    private boolean[][] assignments;
 
+    // above is bitmask
+    private void solve(int row, int below, int rooks, int specials) {
+        if (row == 0) {
+            assignments[rooks][specials] = true;
+            return;
+        }
+        row--;
+
+        for (int i = -1; i < columns[row]; i++) {
+            int t = 0;
+            for (int j = i + 1; j < columns[row]; j++) {
+                if ((below & 1 << j) == 0) t++;
+            }
+
+            if (i == -1) {
+                solve(row, below, rooks, specials + t);
+            } else if ((below & 1 << i) == 0) {
+                solve(row, below | 1 << i, rooks + 1, specials + t);
+            }
+        }
+    }
+
+    public String describe(int[] cols) {
+        columns = cols;
+        n = cols.length;
+        assignments = new boolean[n + 1][n * 10 + 1];
+
+        solve(n, 0, 0, 0);
+
+        StringBuilder sb = new StringBuilder();
+        for (int r = 0; r < assignments.length; r++) {
+            for (int s = 0; s < assignments[0].length; s++) {
+                if (assignments[r][s]) {
+                    sb.append(String.format("%d,%d ", r, s));
+                }
+            }
+        }
+        if (sb.length() > 0) sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1 << 0);
+        int[] cols = {3,3,3};
+        System.out.println(new WeirdRooks().describe(cols));
+    }
 }
