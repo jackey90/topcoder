5afcff2e05e4e7f115892e92f5d110355c0dc478
jackey90 <jackey90.hj@gmail.com>
2015-10-09 16:57:52 +0800

    valid anagram

--- src/main/java/org/jackey/leetcode/easy/ValidAnagram.java
+++ src/main/java/org/jackey/leetcode/easy/ValidAnagram.java
@@ -2,6 +2,9 @@ package main.java.org.jackey.leetcode.easy;
 
 import org.jackey.kingbox.jtool.inAndOut.JOutPrint;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jackey90 on 15/9/29.
  * <p/>
@@ -17,6 +20,9 @@ import org.jackey.kingbox.jtool.inAndOut.JOutPrint;
 
 
 public class ValidAnagram {
+
+    private Map<Character,Integer> map = new HashMap<Character,Integer>();
+
     public boolean isAnagram(String s, String t) {
         int len = s.length();
 
@@ -25,7 +31,23 @@ public class ValidAnagram {
         }
 
         for(int i = 0; i < len; i++){
-            if(s.charAt(i) != t.charAt(len - 1 - i)){
+            char tmpS = s.charAt(i);
+            char tmpT = t.charAt(i);
+            if(map.containsKey(tmpS)){
+                map.put(tmpS,map.get(tmpS) + 1);
+            }else{
+                map.put(tmpS,1);
+            }
+
+            if(map.containsKey(tmpT)){
+                map.put(tmpT,map.get(tmpT) - 1);
+            }else{
+                map.put(tmpT,-1);
+            }
+        }
+
+        for(Map.Entry<Character,Integer> entry: map.entrySet()){
+            if(entry.getValue() != 0){
                 return false;
             }
         }
@@ -34,6 +56,6 @@ public class ValidAnagram {
     }
 
     public static void main(String[] args) {
-        JOutPrint.JPrint(new ValidAnagram().isAnagram("asdffgh","hgfdsa"));
+        JOutPrint.JPrint(new ValidAnagram().isAnagram("abc","bac"));
     }
 }
