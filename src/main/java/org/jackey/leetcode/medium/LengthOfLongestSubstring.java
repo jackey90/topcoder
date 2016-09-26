aa347923c3d80d1ee7187ccd31caaec5a916e8b0
jackey90 <jackey90.hj@gmail.com>
2016-09-26 19:28:52 +0800

    length of longest substring

---
+++ src/main/java/org/jackey/leetcode/medium/LengthOfLongestSubstring.java
@@ -0,0 +1,46 @@
+package main.java.org.jackey.leetcode.medium;
+
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jackey90 on 16/9/26.
+ */
+public class LengthOfLongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+
+        if (s == null || s.length() <= 0 ){
+            return 0;
+        }
+
+        int max = 0;
+        int start = 0;
+
+        int[] map = new int[256];
+        for(int i = 0; i < map.length; i++){
+            map[i] = -1;
+        }
+        for(int i = 0 ; i < s.length(); i++){
+
+            char a = s.charAt(i);
+            int lastIndex = map[a];
+            if(lastIndex >= start){
+                start = lastIndex + 1;
+            }
+            map[a] = i;
+
+            max = Math.max(i - start + 1, max);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abba"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring("aab"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
