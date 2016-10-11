a9f713b7c53d6fae36cd20d6faacc9c91da9368f
jackey90 <jackey90.hj@gmail.com>
2016-10-11 22:42:57 +0800

    maximum sub array add other solution

--- src/main/java/org/jackey/leetcode/medium/MaximumSubarray.java
+++ src/main/java/org/jackey/leetcode/medium/MaximumSubarray.java
@@ -6,11 +6,82 @@ package main.java.org.jackey.leetcode.medium;
 public class MaximumSubarray {
 
     /**
-     *  for example:  array = [-2,1,-3]
-     *                max = 1
-     *                lMax = -1
-     *                rMax = -2
-     *                sum = -4
+     * O(n) without divide and conquer
+     */
+    public int maxSubArrayOnWithOutDivide1(int[] nums) {
+        int max = nums[0];
+        int iMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            iMax = Math.max(iMax + nums[i], nums[i]);
+            max = Math.max(iMax, max);
+        }
+        return max;
+    }
+
+    /**
+     * O(n) without divide and conquer
+     */
+    public int maxSubArrayOnWithOutDivide2(int[] nums) {
+        int max = nums[0];
+        int iMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            iMax += nums[i];
+            max = Math.max(iMax, max);
+            if(iMax < 0){
+                iMax = 0;
+            }
+        }
+        return max;
+    }
+
+    /**
+     *  using divide and conquer but with O(nLogn)
+     * @return
+     */
+    public int maxSubArrayOnlognUseDivide(int[] A,int left, int right){
+        if(left == right){return A[left];}
+        int mid = left + (right - left) / 2;
+        int leftSum = maxSubArrayOnlognUseDivide(A, left, mid);// left part
+        int rightSum = maxSubArrayOnlognUseDivide(A,mid+1,right);//right part
+        int crossSum = crossSubarray(A,left,right);// cross part
+        if(leftSum >= rightSum && leftSum >= crossSum){// left part is max
+            return leftSum;
+        }
+        if(rightSum >= leftSum && rightSum >= crossSum){// right part is max
+            return rightSum;
+        }
+        return crossSum; // cross part is max
+    }
+
+
+    public int crossSubarray(int[] A,int left,int right){
+        int leftSum = Integer.MIN_VALUE;
+        int rightSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int mid = left + (right - left) / 2;
+        for(int i = mid; i >= left ; i--){
+            sum = sum + A[i];
+            if(leftSum < sum){
+                leftSum = sum;
+            }
+        }
+        sum = 0;
+        for(int j = mid + 1; j <= right; j++){
+            sum = sum + A[j];
+            if(rightSum < sum){
+                rightSum = sum;
+            }
+        }
+        return leftSum + rightSum;
+    }
+
+    //************  O(n)  divide and conquer
+    /**
+     * for example:  array = [-2,1,-3]
+     * max = 1
+     * lMax = -1
+     * rMax = -2
+     * sum = -4
      */
     private class ArrayContext {
         int max; // max sum of the sub array
@@ -57,10 +128,10 @@ public class MaximumSubarray {
     }
 
 
-
     public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1};
-        System.out.println(new MaximumSubarray().maxSubArray(nums));
+        int[] nums = {-1,  -2,-3};
+        System.out.println(new MaximumSubarray().maxSubArrayOnWithOutDivide1(nums));
+        System.out.println(new MaximumSubarray().maxSubArrayOnWithOutDivide2(nums));
     }
 }
 
